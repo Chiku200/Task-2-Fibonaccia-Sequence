cd8bd418cb63acd4bb00f7b60cf9350583262db7
Kiran kumar Mandal <92847355+Chiku200@users.noreply.github.com>
2022-08-26 10:15:38 +0530

    Add files via upload

---
+++ FibonacciSequence.py
@@ -0,0 +1,17 @@
+Fibo=int(input("Enter the number :"))
+a=0
+b=1
+c=0
+if Fibo<=0:
+    print ('Please enter number grater than 0')
+else:
+    for i in range(0,Fibo):
+       print(c, end=" ")
+       a=b
+       b=c
+       c=a+b
+
+
+
+
+
